c334546468c6a2029b6610b805082643863c2a44
Shailesh Shrestha <102371028+Shailesh1Shrestha@users.noreply.github.com>
2022-10-13 17:53:51 +0545

    Update generatePassword.py

--- generatePassword.py
+++ generatePassword.py
@@ -1,6 +1,6 @@
 import random
 
-passlen = int(input("Enter the length of password \n"))
-s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-p = "".join(random.sample(s,passlen))
-print(p)
+length = int(input("Enter the length of password \n"))
+c = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+pass = "".join(random.sample(c,length))
+print(pass)
